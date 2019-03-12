257e8a3a250a5704a3517ded6f844e4a5d1ba6d0
yamPerson <48094779+yamPerson@users.noreply.github.com>
2019-03-11 19:48:28 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-var text = prompt('How many digits is your password?');
 var random = Math.floor(Math.random() * 9)+1;
+var text = prompt('How many digits is your password? (Number between 4 and 10)');
+var num = parseInt(text);
+
+if 
+
