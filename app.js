009e4df2bac4eedc7bc9a93d512cc2ce03974f89
yamPerson <48094779+yamPerson@users.noreply.github.com>
2019-03-13 08:40:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+var digit = prompt('How many digits is your password?');
+var num = parseInt(digit);
+var times = prompt('How many do you want to see?');
+var time = parseInt(times);
+
 function random() {
   return Math.floor(Math.random() * 9) + 1;
 }
 
-for (i=0; i<100; i++) {
-  document.getElementsByClassName('password')[0].innerHTML = random();
+for (i=0; i<times; i++) {
+  document.write('<br>')
+  for (a=0; a<num; a++) {
+    document.write(random());
+  }
 }
