22d55fa930d9523833a468c231b04e796319a95d
yamPerson <48094779+yamPerson@users.noreply.github.com>
2019-03-14 11:50:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
-//var lOrN = prompt('Do
+var lOrN = prompt('Do you want a letter or number password? (l/n));
 
 function random() {
   return Math.floor(Math.random() * 9) + 1;
+}         
+
+function enter() {
+  var chars = prompt('Enter characters');
+  return chars;
 }
+                  
+if (lOrN === 'n') {
+  
+  var digit = prompt('How many digits is your password?');
+  var num = parseInt(digit);
+  var times = prompt('How many do you want to see?');
+  var time = parseInt(times);
 
-var digit = prompt('How many digits is your password?');
-var num = parseInt(digit);
-var times = prompt('How many do you want to see?');
-var time = parseInt(times);
-
-
-for (i=0; i<times; i++) {
-  document.write('<br>')
-  for (a=0; a<num; a++) {
-    document.write(random());
+  for (i=0; i<times; i++) {
+    document.write('<br>')
+    for (a=0; a<num; a++) {
+      document.write(random());
+    }
   }
-}
+} else { 
+  var number = prompt('How many characters do you want to add?');
+  var characters = [''];
+  
+  for (i=0; i<number; i++) {
+    enter()
+    characters.push(chars);
+    document.write('<br>');
+    document.write(characters);
+  }
+  
