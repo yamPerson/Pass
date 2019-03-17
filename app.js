e62ebb59e23096dc4cfe1eb935f1767fc0e3f026
yamPerson <48094779+yamPerson@users.noreply.github.com>
2019-03-16 20:48:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-function random() {
-  return Math.floor(Math.random() * 11);
-}         
+// function random() {
+//   return Math.floor(Math.random() * 11);
+// }         
  
-var digit = prompt('How many digits do you want your password to be?');
-var num = parseInt(digit);
-var times = prompt('How many do you want to see?');
-var time = parseInt(times);
+// var digit = prompt('How many digits do you want your password to be?');
+// var num = parseInt(digit);
+// var times = prompt('How many do you want to see?');
+// var time = parseInt(times);
 
-  for (i=0; i<times; i++) {
-    document.write('<br>')
-    for (a=0; a<num; a++) {
-      document.write(random());
-    }
-  }
+//   for (i=0; i<times; i++) {
+//     document.write('<br>')
+//     for (a=0; a<num; a++) {
+//       document.write(random());
+//     }
+//   }
   
